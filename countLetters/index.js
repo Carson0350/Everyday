dbd9c1feae5fbf7d49e27d1ab07069ba451721bf
Carson0350 <carson.workflow@gmail.com>
2020-08-20 12:20:51 -0600

    countLetters

---
+++ countLetters/index.js
@@ -0,0 +1,18 @@
+function countLetters(str) {
+  var results = {};
+  str = str.toLowerCase();
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 97 &&  str.charCodeAt(i) <= 122) {
+      if (results[str[i]] > 0) {
+        results[str[i]] += 1;
+      }
+      else {
+        results[str[i]] = 1;
+      }
+    }
+  }
+
+  return JSON.stringify(results);
+}
+
+console.log(countLetters("Hello World"));
