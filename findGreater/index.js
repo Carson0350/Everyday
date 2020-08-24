6dda1b02a773fbb9a080a49f8fca31699572821b
Carson0350 <carson.workflow@gmail.com>
2020-08-24 10:38:54 -0600

    findGreater

---
+++ findGreater/index.js
@@ -0,0 +1,24 @@
+
+/*
+# Find Greater
+
+Write a function that takes two arguments. The first argument is an array of numbers. The second argument is a number to compare with each item in the array. Your function should return an array of numbers that are greater than the second argument.
+
+Example:
+
+findGreater([1,2,3,4,5,6,7,8], 5);
+// => [6,7,8]
+
+## Input
+
+2 inputs:
+
+An array of numbers [1, 2, 3, 4, 5, 6, 7, 8] and a number to compare each number with
+
+## Output
+
+An array of numbers that are greater than the second argument (e.g. [6,7,8])
+ */
+
+
+
