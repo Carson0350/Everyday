19e2218163e98c62578d0a6f03da06b1fbf858a9
Carson0350 <carson.workflow@gmail.com>
2020-08-24 10:39:29 -0600

    findGreater Solution

--- findGreater/index.js
+++ findGreater/index.js
@@ -20,5 +20,12 @@ An array of numbers [1, 2, 3, 4, 5, 6, 7, 8] and a number to compare each number
 An array of numbers that are greater than the second argument (e.g. [6,7,8])
  */
 
+function findGreater (array, target) {
+  array.filter((element) => {
+    return element > target
+  })
+}
+
+console.log(findGreater([4,7,8,9,2,4,7,10,22,45,89,45,76,34,34], 12))
 
 
