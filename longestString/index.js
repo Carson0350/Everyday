a1feeaef399dfd67bea0da7c05173f791782b230
Carson0350 <carson.workflow@gmail.com>
2020-09-01 07:20:42 -0600

    update instructions

--- longestString/index.js
+++ longestString/index.js
@@ -1,4 +1,16 @@
-// Created by Elana Kopelevich
+/*
+# Longest String
+
+Write a function that returns the longest string in the array and it's length as an object.
+
+## Input
+
+An array of strings (e.g ["truck", "sidewalk", "book"])
+
+## Output
+
+An object with the key of the longest string and a value of the length of the string (e.g. { sidewalk: 8 })
+*/
 
 var input = ['truck', 'sidewalk', 'book'];
 
@@ -21,3 +33,4 @@ function longestString(array){
 }
 
 longestString(input);
+
